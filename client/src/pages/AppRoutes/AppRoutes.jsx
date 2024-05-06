c8d02fed7dc7492f6f7dd7d34f89029cfe841374
Bushak <jindra2004@seznam.cz>
2024-05-06 12:01:20 +0200

    Created design

--- client/src/pages/AppRoutes/AppRoutes.jsx
+++ client/src/pages/AppRoutes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import Home from "../Home/Home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
+import Design from "../Design/Design";
 
 export default function AppRoutes(){
     return(
@@ -11,6 +12,7 @@ export default function AppRoutes(){
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/contact" element={<Contact/>}/>
+            <Route path="/design" element={<Design/>}/>
         </Routes>
         </BrowserRouter>
         </>
